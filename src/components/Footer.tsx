689c6386b8bb9e8ba134affe16cbb5d0d3830215
Anders Døssing Jepsen <anders.d.jepsen@gmail.com>
2026-03-02 13:56:14 +0100

    opdatering af tekst i footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,9 +18,8 @@ export default function Footer() {
           .
         </p>
         <p>
-          Som udgiver af podcasten &apos;Hvid Røg&apos;, som har en satirisk karakter, tilfalder ophavsretten på denne side Jyllands-Posten,
-          som dog ikke står til ansvar for at al information på siden er korrekt.{' '}
-          © Alt materiale på denne side er omfattet af gældende lov om ophavsret.{' '}
+          © Som udgiver af podcasten &apos;Hvid Røg&apos; tilfalder ophavsretten for indholdet på denne side Jyllands-Posten.{' '}
+          Alt materiale på denne side er omfattet af gældende lov om ophavsret. {' '}
           <a
             href="https://jyllands-posten.dk/ophavsret"
             target="_blank"
