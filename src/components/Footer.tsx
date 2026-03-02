c529d9253bdce1816405939b6cde28eda0939278
Anders Døssing Jepsen <anders.d.jepsen@gmail.com>
2026-03-02 22:05:03 +0100

    links til værternes profiler

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,22 @@ export default function Footer() {
     <footer className="border-t border-white/10 mt-12 py-8 px-4">
       <div className="max-w-6xl mx-auto space-y-4 text-white/40 text-xs text-center">
         <p>
-          Sture Sandø og Michael &apos;Jøden&apos; Mühlenbach står bag podcasten &apos;Hvid Røg&apos;, som leverer indhold til denne side.
+          <a
+            href="https://www.linkedin.com/in/sture-sand%C3%B8-5a236567/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white/70 underline underline-offset-2 transition-colors"
+          >
+            Sture Sandø
+          </a>{' '}og{' '}
+          <a
+            href="https://www.facebook.com/michael.m.christiansen"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white/70 underline underline-offset-2 transition-colors"
+          >
+            Michael &apos;Jøden&apos; Mühlenbach Christiansen
+          </a>{' '}står bag podcasten &apos;Hvid Røg&apos;, som leverer indhold til denne side.
           Siden er gjort tilgængelig på nettet af{' '}
           <a
             href="https://www.linkedin.com/in/andersdjepsen/"
